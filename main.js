9da1ec8ceb142858826c96b360a06fe3b7bfd5fa
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2019-05-07 07:56:39 -0700

    Working on the sticky menu

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 /*jslint browser: true*/
-/*global $, document, window*/
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+/*global $, window*/
+$("#mainmenu").css("border", "3px solid red");
 
 // Get the navbar
-var navbar = document.getElementById("navbar");
+var navbar = $("#mainmenu");
+console.log(navbar.id)
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var sticky = $("#mainmenu").offsetTop;
+console.log(sticky)
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.addClass("sticky")
   } else {
-    navbar.classList.remove("sticky");
+    navbar.removeClass("sticky");
   }
 }
+
+
+window.onload = function() {
+
+
+    // When the user scrolls the page, execute myFunction
+    window.onscroll = function() {myFunction()};
+};
