824efbba69dace4ff8d8d07d3fc042a8836901d7
AnteusNar <jedidiah.h.wallace@gmail.com>
2019-05-08 13:01:18 -0700

    cleaning and added comment for plan

--- static/js/main.js
+++ static/js/main.js
@@ -5,14 +5,16 @@
 
 $(document).ready(function () {
 
+    // need to build a script to get the height of the navbar and set an offset for the #links.....
+
     // Colorbox iframes
 
     $("#bbb").colorbox({iframe: true, href: "https://www.bbb.org/us/nv/north-las-vegas/profile/locksmith/atlas-integrated-security-llc-1086-90048263", width: "80%", height: "80%"});
 
-    $("#statelic").colorbox({iframe: true, href: "https://app.nvcontractorsboard.com/Clients/NVSCB/Public/Shared/Details.aspx?EntID=3951056&LicNum=270936", width: "80%", height: "80%"})
+    $("#statelic").colorbox({iframe: true, href: "https://app.nvcontractorsboard.com/Clients/NVSCB/Public/Shared/Details.aspx?EntID=3951056&LicNum=270936", width: "80%", height: "80%"});
 
   // Custom
-  var stickyToggle = function(sticky, stickyWrapper, scrollElement) {
+  var stickyToggle = function (sticky, stickyWrapper, scrollElement) {
     //used to get width of parent for use with bootsrap 4 grid
     var width = sticky.parent().css("width");
     var stickyHeight = sticky.outerHeight();
